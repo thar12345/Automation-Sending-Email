6259be7c0afe9a6b652a20f85d89ab66f10011bd
thar12345 <tharsihan019@gmail.com>
2023-08-12 16:45:00 -0400

    Add spell check function using Grammarbot API

--- sendEmail.py
+++ sendEmail.py
@@ -5,6 +5,30 @@ from email.message import EmailMessage
 import requests
 import config
 
+def spell_check(text):
+    url = "https://grammarbot.p.rapidapi.com/check"
+
+    payload = {
+        "text": text,
+        "language": "en-US"
+    }
+    headers = {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": config.key,
+        "X-RapidAPI-Host": "grammarbot.p.rapidapi.com"
+    }
+
+    response = requests.post(url, data=payload, headers=headers)
+
+    
+    try:
+        print(response.json()['matches'][0]['message'])
+        return True
+    except:
+        return False
+
+
+
 db = mysql.connector.connect(
     host="localhost",
     user="root",
